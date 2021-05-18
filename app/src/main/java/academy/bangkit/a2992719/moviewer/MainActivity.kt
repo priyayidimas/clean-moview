0ee73203dc37772d225f0c2316f957f5f90a4a01
priyayidimas <priyayidimas@upi.edu>
2021-05-18 10:41:00 +0700

    Project Init

---
+++ app/src/main/java/academy/bangkit/a2992719/moviewer/MainActivity.kt
@@ -0,0 +1,11 @@
+package academy.bangkit.a2992719.moviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
